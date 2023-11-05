760ace0e6c0bfb9a90f39ca5f0dd8c2cdf70b289
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-05 15:50:38 +0700

    [UPDATE] integrating detail movie

--- src/Pages/DetailRecipe/index.jsx
+++ src/Pages/DetailRecipe/index.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 import YouTube from 'react-youtube'
 import Navbar from '../../Components/Navbar'
 import Footer from '../../Components/Footer'
+import { useParams } from 'react-router-dom'
+import Loading from '../../Components/Loading'
+import Error404 from '../../Components/Error404'
 
 const style = {
   h1: {
@@ -29,31 +32,44 @@ export default function DetailRecipe() {
   const [utils, setUtils] = React.useState([])
   const [advice, setAdvice] = React.useState([])
   const [comments, setComments] = React.useState([])
+  const [loading, setLoading] = React.useState(undefined)
+
+  const { slug } = useParams()
 
   const initpage = async () => {
     try {
+      setLoading(true)
       if (Object.keys(foodDetail).length === 0) {
         const food = await axios({
-          method: 'get',
-          url: 'http://localhost:3001/recipes/fb8a5074-6e2b-4b42-8320-7b11419cdc2b'
+          method: 'post',
+          url: `${String(window.env.BE_URL)}/recipes/${slug}/detail`,
+          data: {
+            title: String(slug?.split('-').join(' '))
+          }
         })
-        setFoodDetail(food.data.data[0])
-        setIngredient(food.data.data[0].ingredients.ingridient)
-        setSteps(food.data.data[0].ingredients.steps)
-        setAdvice(food.data.data[0].ingredients.advice)
-        setUtils(food.data.data[0].ingredients.utils)
-      }
 
-      if (Object.keys(comments).length === 0) {
-        const comment = await axios({
-          method: 'get',
-          url: 'http://localhost:3001/recipes/fb8a5074-6e2b-4b42-8320-7b11419cdc2b/detail/comments'
-        })
-        setComments(comment.data.data)
+        console.log(String(slug?.split('-').join(' ')))
+        setFoodDetail(food?.data?.data[0])
+
+        setIngredient(food?.data?.data[0]?.ingredients?.ingridient)
+        setSteps(food?.data?.data[0]?.ingredients?.steps)
+        setAdvice(food?.data?.data[0]?.ingredients?.advice)
+        setUtils(food?.data?.data[0]?.ingredients?.utils)
+
+        if (food.data.data[0].receipt_uid) {
+          const comment = await axios({
+            method: 'get',
+            url: `${window.env.BE_URL}/recipes/${food.data.data[0].receipt_uid}/detail/comments`
+          })
+          console.log(comment)
+          setComments(comment.data.data)
+        }
       }
 
     } catch (error) {
       console.log(error)
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -61,89 +77,106 @@ export default function DetailRecipe() {
     initpage()
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [foodDetail, comments])
+  }, [foodDetail, comments, loading])
+
+  console.log(foodDetail)
 
   return (
     <div>
       <Navbar />
-      <article id='foodRecipeDetail' className='container d-flex flex-column' style={{ padding: '3vh 10vw 3vh 10vw' }}>
-        <section className='container d-flex flex-column m-auto'>
+      {
+        loading === undefined ? "" :
+          loading === true ? <Loading /> :
+            !(foodDetail?.hasOwnProperty('id')) ?
+              <div className='d-flex flex-column justify-content-center align-items-center' style={{height: '43vh'}}>
+                <Error404 />
+                <p style={{fontWeight: 600}}>We're Sorry, Page you want to visit not found.</p>
+              </div> :
+              <article id='foodRecipeDetail' className='container d-flex flex-column' style={{ padding: '3vh 10vw 3vh 10vw' }}>
+                <section className='container d-flex flex-column m-auto'>
 
-          <h1 style={style.h1}>{foodDetail.title}</h1>
+                  <h1 style={style.h1}>{foodDetail?.title}</h1>
 
-          <div className='mb-1 m-auto'>
-            <figure style={{ textAlign: 'center', maxWidth: '600px', margin: '0px 0px 30px 0px' }}>
-              <img className='my-4' style={{ borderRadius: '20px', width: '100%' }} src={foodDetail.image} alt="foodDetail.title" />
-              <figcaption>{foodDetail.title}</figcaption>
-            </figure>
-          </div>
+                  <div className='mb-1 m-auto'>
+                    <figure style={{ textAlign: 'center', maxWidth: '600px', margin: '0px 0px 30px 0px' }}>
+                      <img className='my-4' style={{ borderRadius: '20px', width: '100%' }} src={foodDetail?.image} alt="foodDetail.title" />
+                      <figcaption>{foodDetail?.title}</figcaption>
+                    </figure>
+                  </div>
 
-          <div>
-            <h3>Ingredients</h3>
-            <ul>
-              {ingredients.map(
-                (ingredient) => <li>{ingredient}</li>
-              )}
-            </ul>
-          </div>
+                  <div>
+                    <h3>Ingredients</h3>
+                    <ul>
+                      {ingredients?.map(
+                        (ingredient) => <li>{ingredient}</li>
+                      )}
+                    </ul>
+                  </div>
 
-          <div>
-            <h3>Utils</h3>
-            <ul>
-              {utils.map(
-                (tool) => <li>{tool}</li>
-              )}
-            </ul>
-          </div>
+                  <div>
+                    <h3>Utils</h3>
+                    <ul>
+                      {utils?.map(
+                        (tool) => <li>{tool}</li>
+                      )}
+                    </ul>
+                  </div>
 
-          <div>
-            <h3>Steps</h3>
-            <ul>
-              {steps.map(
-                (step) => <li>{step}</li>
-              )}
-            </ul>
-          </div>
+                  <div>
+                    <h3>Steps</h3>
+                    <ul>
+                      {steps?.map(
+                        (step) => <li>{step}</li>
+                      )}
+                    </ul>
+                  </div>
 
-          <div>
-            <h3>Advice</h3>
-            <ul>
-              {advice.map(
-                (sugest) => <li>{sugest}</li>
-              )}
-            </ul>
-          </div>
+                  <div>
+                    <h3>Advice</h3>
+                    <ul>
+                      {advice?.map(
+                        (sugest) => <li>{sugest}</li>
+                      )}
+                    </ul>
+                  </div>
 
-          <div className='mt-2'>
-            <YouTube style={{ maxWidth: '60%' }} videoId={String(foodDetail.video_url).split('=')[1]} />
-          </div>
+                  <div className='mt-2 desktop-component'>
+                    <YouTube videoId={foodDetail?.video_url?.split('=')[1]} />
+                  </div>
 
-        </section>
+                  <div>
+                    <button className='btn my-2 shadow-sm' style={style.comentBtn}>
+                      Comment
+                    </button>
+                  </div>
 
-        <div id='form-coment' className='text-center my-5 m-auto' style={{ width: '900px', maxWidth: '90%' }}>
-          <textarea className='form-control mb-2' rows="3" style={{ backgroundColor: '#F6F5F4' }} />
-          <button className='btn my-2 shadow-sm' style={style.comentBtn}>
-            Comment
-          </button>
-        </div>
+                </section>
 
-        <div >
-          <h2 className='mb-4'>Comments</h2>
-          {comments.length === 0 ?
-            "No Comment Found" :
-            comments.map((item) => (
-              <div className='d-flex flex-row gap-3 mt-3'>
-                <div>
-                  <img style={{ width: '64px', borderRadius: '50%' }} src="/profile.svg" alt="profile" />
+                <div id='form-coment' className='text-center my-5 m-auto' style={{ width: '900px', maxWidth: '90%' }}>
+                  <textarea className='form-control mb-2' rows="3" style={{ backgroundColor: '#F6F5F4' }} />
+                  <button className='btn my-2 shadow-sm' style={style.comentBtn}>
+                    Comment
+                  </button>
                 </div>
-                <div>
-                  <p>Username</p>
-                  <p>Berkata "{item.message}"</p>
+
+                <div >
+                  <h2 className='mb-4' style={{ fontWeight: 600 }}>Comments</h2>
+                  {comments.length === 0 ?
+                    "No Comment Found" :
+                    comments?.map((item) => (
+                      <div className='d-flex flex-row gap-3 mt-3'>
+                        <div>
+                          <img style={{ width: '64px', borderRadius: '50%' }} src={item.photo} alt="profile" />
+                        </div>
+                        <div>
+                          <p><strong>{item.name}</strong></p>
+                          <p>Berkata "{item.message}"</p>
+                        </div>
+                      </div>
+                    ))}
                 </div>
-              </div>
-            ))}
-        </div>
-      </article>
+              </article>
+      }
       <Footer />
     </div>
   )
