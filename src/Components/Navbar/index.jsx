5989a35ac96a4cbba3d894330fa02caaebf3f86b
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-08 21:40:43 +0700

    [UPDATE] update conditional rendering on navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -11,6 +11,17 @@ const style = {
 }
 
 export default function Navbar() {
+	const [profile, setProfile] = React.useState({})
+
+	React.useEffect(() => {
+
+		if (localStorage.getItem('token') && localStorage.getItem('user')) {
+			setProfile(JSON.parse(localStorage.getItem('user')))
+		}
+
+	}, [])
+
+
 	return (
 		<>
 			<nav className='container d-flex justify-content-between my-4 align-items-center'>
@@ -18,7 +29,7 @@ export default function Navbar() {
 				<div className='nav-left d-flex gap-2 align-items-center'>
 
 					<Link to='/'>
-						<div className='nav-container d-flex align-items-center'>
+						<div className='nav-container d-flex align-items-center' style={{ height: '64px', borderRadius: '30px' }}>
 							<img className='shadow-sm' style={{ height: '64px', borderRadius: '30px' }} src="/logo-h.svg" alt="logo" />
 						</div>
 					</Link>
@@ -39,21 +50,92 @@ export default function Navbar() {
 					</Link>
 				</div>
 
-				<div className='nav-right'>
+				<div className='nav-right d-flex'>
 					{/* Desktop Menu */}
-					<Link to='/user/login'>
-						<div className='nav-container shadow-sm p-2 d-flex align-items-center gap-2 desktop-component'
-							style={{ borderRadius: '36px', backgroundColor: 'white' }}>
-							<span style={style.navLeftLink} className='nav-right-link'>Login</span>
-							<img className='shadow-sm' style={{ height: '48px', borderRadius: '50%' }} src="/profile.svg" alt="logo" />
-						</div>
-					</Link>
+					{
+						!localStorage.getItem('token') && !localStorage.getItem('user') ?
+							<Link to='/user/login'>
+								<div className='nav-container shadow-sm p-2 d-flex align-items-center gap-2 desktop-component'
+									style={{ borderRadius: '36px', backgroundColor: 'white' }}>
+									<span style={style.navLeftLink} className='nav-right-link'>Login</span>
+									<img className='shadow-sm' style={{ height: '48px', borderRadius: '50%' }} src="/profile.svg" alt="logo" />
+								</div>
+							</Link> :
+
+							<div class="btn-group">
+								<div className='nav-container shadow-sm p-2 d-flex align-items-center gap-2 desktop-component' data-bs-toggle="dropdown"
+									style={{ borderRadius: '36px', backgroundColor: 'white' }}>
+									<div>
+										<span style={style.navLeftLink} className='nav-right-link'>Hi, {profile.first_name}</span>
+									</div>
+									<img className='shadow-sm' style={{ height: '48px', borderRadius: '50%' }} src={profile?.photo_profile} alt="logo" />
+								</div>
+								<ul class="dropdown-menu dropdown-menu-lg-end desktop-component">
+									<li><button class="dropdown-item shadow-sm text-center" type="button"
+										onClick={() => {
+											setProfile({})
+											localStorage.removeItem('user')
+											localStorage.removeItem('token')
+										}}>Logout</button></li>
+								</ul>
+							</div>
+
+
+
+					}
 					{/* End Of Desktop Menu */}
 					{/* Burger Menu */}
-					<div className='nav-container shadow-sm mobile-component p-2'
-						style={{ height: '54px', width: '54px', backgroundColor: 'white', borderRadius: '50%' }}>
-						<img style={{ width: '100%' }} src="/assets/icons/burger-menu.svg" alt="logo" />
-					</div>
+
+					{!localStorage.getItem('token') && !localStorage.getItem('user') ?
+						<div class="btn-group">
+							<div className='nav-container shadow-sm p-2 d-flex align-items-center gap-2 mobile-component' data-bs-toggle="dropdown"
+								style={{ borderRadius: '36px', backgroundColor: 'white' }}>
+								<img className='shadow-sm' style={{ height: '48px', borderRadius: '50%' }} src='/profile.svg' alt="logo" />
+							</div>
+							<ul class="dropdown-menu dropdown-menu-lg-end">
+								<li className=''>
+									<Link to='/user/login' >
+										<button class="dropdown-item shadow text-center my-1 mobile-component" type="button" >Login</button>
+									</Link>
+								</li>
+								<li className=''>
+									<Link to='/user/register' >
+										<button class="dropdown-item shadow text-center my-1 mobile-component" type="button" >Register</button>
+									</Link>
+								</li>
+							
+							</ul>
+						</div> :
+						<div class="btn-group">
+							<div className='nav-container shadow-sm p-2 d-flex align-items-center gap-2 mobile-component' data-bs-toggle="dropdown"
+								style={{ borderRadius: '36px', backgroundColor: 'white' }}>
+								<img className='shadow-sm' style={{ height: '48px', borderRadius: '50%' }} src={profile?.photo_profile} alt="logo" />
+							</div>
+							<ul class="dropdown-menu dropdown-menu-lg-end">
+								<li>
+									<Link style={{textAlign: 'center'}} to='/user/profile' >
+										<button class="dropdown-item shadow text-center my-1 mobile-component" type="button" ><div>Profile</div></button>
+									</Link>
+								</li>
+								<li>
+									<Link  to='/user/recipes' >
+										<button class="dropdown-item shadow text-center my-1 mobile-component" type="button" >Add Recipe</button>
+									</Link>
+								</li>
+								<li>
+									<Link to='/' >
+										<button class="dropdown-item shadow text-center my-1 mobile-component" type="button" >Home</button>
+									</Link>
+								</li>
+								<li><button class="dropdown-item shadow text-center my-1 mobile-component" type="button"
+									onClick={() => {
+										setProfile({})
+										localStorage.removeItem('user')
+										localStorage.removeItem('token')
+									}}>Logout</button></li>
+							</ul>
+						</div>
+					}
 					{/* End of Burger Menu */}
 				</div>
 
