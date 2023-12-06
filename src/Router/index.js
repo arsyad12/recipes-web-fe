0006cdb8c14280dc0433939cd07e680760338f5f
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-12-06 20:52:10 +0700

    ✅ Add page search

--- src/Router/index.js
+++ src/Router/index.js
@@ -3,11 +3,16 @@ import DetailRecipe from "../Pages/DetailRecipe";
 import Home from "../Pages/Home";
 import Login from "../Pages/Login";
 import Register from "../Pages/Register";
+import SearchRecipe from "../Pages/SearchRecipe";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Home />,
+	},
+	{
+		path: "/search",
+		element: <SearchRecipe />,
 	},
 	{
 		path: "/detail/:slug",
